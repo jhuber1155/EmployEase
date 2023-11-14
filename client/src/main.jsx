fc01ab5782ca140da2c082592e6722c39b3bfca7
afong <adam.y.w.fong@gmail.com>
2023-11-14 11:59:39 -0800

    resaved app, vite.config, and main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/addJob',
+        element: <AddJob />
+      }, {
+        path: '/jobs/:jobid',
+        element: <Jobs />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
