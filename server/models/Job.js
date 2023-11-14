a8ecaa732b08dfc9f93c9e6a8d5c1792f392cd70
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-11-14 13:26:14 -0800

    Added models

---
+++ server/models/Job.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const jobSchema = new Schema({
+    jobId: {
+      type: Number
+    },
+    jobTitle: { 
+      type: String
+    },
+    companyName: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    appliedOn: {
+      type: Date,
+      default: Date.now,
+      get: (date) => formate_date(date)
+    },
+    interviewOffered: {
+      type: Boolean
+    },
+    status: {
+      type: String
+    }
+  });
+
+  const Job = model('Job', userSchema);
+
+module.exports = Job;
