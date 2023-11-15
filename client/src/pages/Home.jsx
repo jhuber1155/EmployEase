5e6d51ddb083f714bc7c6364025055efbd1b3b8d
afong <adam.y.w.fong@gmail.com>
2023-11-15 14:25:21 -0800

    fixed process missing issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,27 +1,9 @@
 import { useQuery } from '@apollo/client';
-// import { QUERY_JOBS } from '../../utils/queries';
-// import JobBoard from '../components/JobBoard.jsx';
+import { QUERY_ME } from '../utils/queries';
+import JobBoard from '../components/JobBoard.jsx';
 
 const Home = () => {
-  const {loading, data } = {
-    loading: false,
-    data: {
-      jobs: [
-        {
-          _id: 1,
-          jobTitle: 'Salaryman',
-          salary: '10,000',
-          companyName: 'Reebok'
-        },
-        {
-          _id: 2,
-          jobTitle: 'Businessman',
-          salary: '20,000',
-          companyName: 'Busyland'
-        },
-      ]
-    }
-  }; //useQuery(QUERY_JOBS);
+  const {loading, data } = useQuery(QUERY_ME);
   const jobs = data?.jobs || [];
 
   return (
@@ -29,14 +11,7 @@ const Home = () => {
       {loading ? (
             <div>Loading...</div>
           ) : (
-            jobs.map((job) => (
-              <div key={job._id}>
-                Title: {job.jobTitle}\n
-                Salary: {job.salary}\n
-                Company: {job.companyName}
-              </div>
-            ))
-            // <JobBoard  jobs={jobs} />  Comment out to test if data is populated
+             <JobBoard  jobs={jobs} />
           )}
     </main>
   )
