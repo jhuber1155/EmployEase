9d9601e8e5796e43047576ba34b75f4ce03d9505
Nathan Hoang <sliqq123@gmail.com>
2023-11-16 12:31:40 -0800

    added footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,21 @@
 import React from 'react';
-import { FaGithub} from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaConnectdevelop } from 'react-icons/fa';
+import {CgMail} from 'react-icons/cg'
+const Footer = () => {
+  return (
+    <footer className=" bg-blue-500 text-white text-center my-4 p-4">
+      <div className="container mx-auto">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} EmployEase
+        </p>
+        <div className="flex justify-center space-x-4 mt-2">
+          <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+            <FaGithub size={20} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
+export default Footer;
