599ff632b2fa9789bddd43830b86955188aeb885
BrianPizz <bpizzimentijr@outlook.com>
2023-11-15 21:55:09 -0800

    job card

--- client/src/pages/Job.jsx
+++ client/src/pages/Job.jsx
@@ -19,17 +19,17 @@ const Job = () => {
                 <div>Loading...</div>
             ) : (
                 <div className="bg-white max-w-2xl mx-auto rounded-md p-8 shadow-md">
-          <h2 className="text-2xl font-semibold mb-4">{jobTitle}</h2>
-          <p className="text-gray-600 mb-2">{companyName}</p>
-          <p className="text-gray-600 mb-2">{location}</p>
-          <p className="text-gray-600 mb-2">{salary}</p>
-          <p className="text-gray-600 mb-2">{fullTime ? 'Full-time' : 'Part-time'}</p>
-          <p className="text-gray-800 mb-4">{description}</p>
-          <a className="text-blue-500 mb-2" href={jobLink}>Link</a>
-          <p className="text-gray-600 mb-2">Status: {status}</p>
-          <p className="text-gray-600 mb-2">Applied On: {appliedOn}</p>
-          <p className="text-gray-600 mb-2">{interviewOffered ? 'Interview Offered' : 'No Interview Offered'}</p>
-        </div>
+                    <h2 className="text-2xl font-semibold mb-4">{jobTitle}</h2>
+                    <p className="text-gray-600 mb-2">{companyName}</p>
+                    <p className="text-gray-600 mb-2">{location}</p>
+                    <p className="text-gray-600 mb-2">{salary}</p>
+                    <p className="text-gray-600 mb-2">{fullTime ? 'Full-time' : 'Part-time'}</p>
+                    <p className="text-gray-800 mb-4">{description}</p>
+                    <a className="text-blue-500 mb-2" href={jobLink}>Link</a>
+                    <p className="text-gray-600 mb-2">Status: {status}</p>
+                    <p className="text-gray-600 mb-2">Applied On: {appliedOn}</p>
+                    <p className="text-gray-600 mb-2">{interviewOffered ? 'Interview Offered' : 'No Interview Offered'}</p>
+                </div>
             )}
         </main>
 
