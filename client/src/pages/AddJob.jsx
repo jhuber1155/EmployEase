5cf16b98b3b73e3b03f209f0f94a1be0819430bd
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-11-16 12:06:58 -0800

    Saving for merge

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -156,7 +156,7 @@ const JobForm = () => {
                         className="block rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                     <label htmlFor="hours" className="ml-2 text-sm text-gray-900">
-                    Is this a Part Time or Full Time Job?
+                    Check the box if the job is a Full Time position.
                     </label>
                     </div>
                 </div>
@@ -232,7 +232,7 @@ const JobForm = () => {
                 Current Status:
               </label>
               <div className="mt-2.5">
-                <input
+                <select
                   type="text"
                   name="status"
                   id="status"
@@ -240,8 +240,11 @@ const JobForm = () => {
                   onChange={handleChange}
                   autoComplete="status"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="What is the current status of this job?"
-                />
+                  placeholder="What is the current status of this job?">
+                  <option value="Open">Open</option>
+                  <option value="Accepted">Accepted</option>
+                  <option value="Rejected">Rejected</option>
+                  </select>
               </div>
             </div>
             </div>
