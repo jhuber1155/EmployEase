96a28aee2173effabcedc353929585ced4250d31
Nathan Hoang <sliqq123@gmail.com>
2023-11-17 11:11:08 -0800

    new styling for login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -38,7 +38,7 @@ const Login = (props) => {
     <div className="flex justify-center">
         <div className="w-full md:w-2/3 lg:w-1/2">
             <div className="card bg-white shadow-lg rounded-lg">
-                <h4 className="card-header bg-gray-800 text-white text-lg font-semibold py-3 px-4 rounded-t-lg">Login</h4>
+                <h4 className="card-header bg-slate-400 text-white text-3xl font-semibold py-3 px-4 rounded-t-lg">Login</h4>
                 <div className="card-body p-4">
                     {data ? (
                         <p>
@@ -49,7 +49,7 @@ const Login = (props) => {
                         <form onSubmit={handleFormSubmit}>
                             <div className="mb-4">
                                 <input
-                                    className="form-control appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    className="form-control appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                     placeholder="Your email"
                                     name="email"
                                     type="email"
@@ -59,7 +59,7 @@ const Login = (props) => {
                             </div>
                             <div className="mb-4">
                                 <input
-                                    className="form-control appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    className="form-control appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                     placeholder="******"
                                     name="password"
                                     type="password"
@@ -68,13 +68,13 @@ const Login = (props) => {
                                 />
                             </div>
                             <button
-                                className="btn bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+                                className="btn bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full"
                                 type="submit"
                             >
                                 Submit
                             </button>
                             <div className="flex justify-center mt-2">
-                                <Link to="/signup" className="text-center text-blue-600 hover:text-blue-800">Signup</Link>
+                                <Link to="/signup" className="text-center text-green-400 hover:text-green-600">Signup</Link>
                             </div>
                         </form>
                     )}
