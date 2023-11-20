f93b9e23a089c58159b2f1858e7652ccb8c4e211
BrianPizz <bpizzimentijr@outlook.com>
2023-11-20 13:44:13 -0800

    job height sizing

--- client/src/pages/Job.jsx
+++ client/src/pages/Job.jsx
@@ -21,7 +21,7 @@ const Job = () => {
 
     if (Auth.loggedIn()) {
         return (
-            <main className='bg-gradient-to-t from-sky-600 to-sky-300 pb-5'>
+            <main className='bg-gradient-to-t from-sky-600 to-sky-300 pb-5 h-screen'>
                 {loading ? (
                     <div>Loading...</div>
                 ) : (
