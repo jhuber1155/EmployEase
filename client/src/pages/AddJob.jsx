8ac9d0b085dbf5aa09b3f17c1328370c20a1a97c
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-11-20 12:28:24 -0800

    Changed location input box for mapping

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -130,10 +130,10 @@ const JobForm = () => {
           />
           <FormInput
             formId="location"
-            title="Location"
+            title="Address (Must include Street Number, City, State, and Zip Code):"
             value={formState.location}
             onChange={handleFormChange}
-            placeholder='Where will this job be located at?'
+            placeholder='123 Main Street, Your City, Your State, 12345'
             labelClasses="block text-sm font-semibold leading-6 text-gray-900"
             inputClasses="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
