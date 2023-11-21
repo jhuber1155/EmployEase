2fc4add17227eca3f25bb3e5d1d70eff12e15079
BrianPizz <bpizzimentijr@outlook.com>
2023-11-21 11:06:05 -0800

    login and signup bg

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -34,61 +34,63 @@ const Login = (props) => {
     };
 
     return (
-<main className="container min-h-screen mx-auto my-4 px-4">
-    <div className="flex justify-center">
-        <div className="w-full md:w-2/3 lg:w-1/2">
-            <div className="card bg-white shadow-lg rounded-lg">
-                <h4 className="card-header bg-slate-400 text-white text-3xl font-semibold py-3 px-4 rounded-t-lg">Login</h4>
-                <div className="card-body p-4">
-                    {data ? (
-                        <p>
-                            Success! You may now head{' '}
-                            <Link to="/" className="text-blue-600 hover:text-blue-800">back to the homepage.</Link>
-                        </p>
-                    ) : (
-                        <form onSubmit={handleFormSubmit}>
-                            <div className="mb-4">
-                                <input
-                                    className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
-                                    placeholder="Your email"
-                                    name="email"
-                                    type="email"
-                                    value={formState.email}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <input
-                                    className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
-                                    placeholder="*********"
-                                    name="password"
-                                    type="password"
-                                    value={formState.password}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <button
-                                className="btn bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full"
-                                type="submit"
-                            >
-                                Submit
-                            </button>
-                            <div className="flex justify-center mt-2">
-                                <Link to="/signup" className="text-center text-green-400 hover:text-green-600">Signup</Link>
-                            </div>
-                        </form>
-                    )}
+        <div className='bg-jobPageBlue'>
+        <main className="container min-h-screen mx-auto py-4 px-4">
+            <div className="flex justify-center">
+                <div className="w-full md:w-2/3 lg:w-1/2">
+                    <div className="card bg-white shadow-lg rounded-lg">
+                        <h4 className="card-header bg-slate-400 text-white text-3xl font-semibold py-3 px-4 rounded-t-lg">Login</h4>
+                        <div className="card-body p-4">
+                            {data ? (
+                                <p>
+                                    Success! You may now head{' '}
+                                    <Link to="/" className="text-blue-600 hover:text-blue-800">back to the homepage.</Link>
+                                </p>
+                            ) : (
+                                <form onSubmit={handleFormSubmit}>
+                                    <div className="mb-4">
+                                        <input
+                                            className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
+                                            placeholder="Your email"
+                                            name="email"
+                                            type="email"
+                                            value={formState.email}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="mb-4">
+                                        <input
+                                            className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
+                                            placeholder="*********"
+                                            name="password"
+                                            type="password"
+                                            value={formState.password}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <button
+                                        className="btn bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full"
+                                        type="submit"
+                                    >
+                                        Submit
+                                    </button>
+                                    <div className="flex justify-center mt-2">
+                                        <Link to="/signup" className="text-center text-green-400 hover:text-green-600">Signup</Link>
+                                    </div>
+                                </form>
+                            )}
 
-                    {error && (
-                        <div className="alert alert-danger mt-3 text-red-600 border border-red-400 rounded bg-red-100 px-4 py-3">
-                            {error.message}
+                            {error && (
+                                <div className="alert alert-danger mt-3 text-red-600 border border-red-400 rounded bg-red-100 px-4 py-3">
+                                    {error.message}
+                                </div>
+                            )}
                         </div>
-                    )}
+                    </div>
                 </div>
             </div>
+        </main>
         </div>
-    </div>
-</main>
 
     );
 };
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -38,67 +38,69 @@ const Signup = () => {
     };
 
     return (
-<main className="container min-h-screen mx-auto my-4 px-4">
-    <div className="flex justify-center">
-        <div className="w-full md:w-2/3 lg:w-1/2">
-            <div className="bg-white shadow-lg rounded-lg">
-                <h4 className="bg-slate-400 text-white text-3xl font-semibold py-3 px-4 rounded-t-lg">Sign Up</h4>
-                <div className="p-4">
-                    {data ? (
-                        <p>
-                            Success! You may now head{' '}
-                            <Link to="/" className="text-blue-600 hover:text-blue-800">
-                                back to the homepage.
-                            </Link>
-                        </p>
-                    ) : (
-                        <form onSubmit={handleFormSubmit}>
-                            <div className="mb-4">
-                                <input
-                                    className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
-                                    placeholder="Your username"
-                                    name="username"
-                                    type="text"
-                                    value={formState.username}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <input
-                                    className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
-                                    placeholder="Your email"
-                                    name="email"
-                                    type="email"
-                                    value={formState.email}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <input
-                                    className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
-                                    placeholder="*********"
-                                    name="password"
-                                    type="password"
-                                    value={formState.password}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <button className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full" type="submit">
-                                Submit
-                            </button>
-                        </form>
-                    )}
+        <div className='bg-jobPageBlue'>
+            <main className="container min-h-screen mx-auto py-4 px-4">
+                <div className="flex justify-center">
+                    <div className="w-full md:w-2/3 lg:w-1/2">
+                        <div className="bg-white shadow-lg rounded-lg">
+                            <h4 className="bg-slate-400 text-white text-3xl font-semibold py-3 px-4 rounded-t-lg">Sign Up</h4>
+                            <div className="p-4">
+                                {data ? (
+                                    <p>
+                                        Success! You may now head{' '}
+                                        <Link to="/" className="text-blue-600 hover:text-blue-800">
+                                            back to the homepage.
+                                        </Link>
+                                    </p>
+                                ) : (
+                                    <form onSubmit={handleFormSubmit}>
+                                        <div className="mb-4">
+                                            <input
+                                                className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
+                                                placeholder="Your username"
+                                                name="username"
+                                                type="text"
+                                                value={formState.username}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                        <div className="mb-4">
+                                            <input
+                                                className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
+                                                placeholder="Your email"
+                                                name="email"
+                                                type="email"
+                                                value={formState.email}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                        <div className="mb-4">
+                                            <input
+                                                className="appearance-none block w-full px-3 py-2 border bg-slate-100 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm"
+                                                placeholder="*********"
+                                                name="password"
+                                                type="password"
+                                                value={formState.password}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                        <button className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full" type="submit">
+                                            Submit
+                                        </button>
+                                    </form>
+                                )}
 
-                    {error && (
-                        <div className="text-red-600 border border-red-400 rounded bg-red-100 px-4 py-3 mt-3">
-                            {error.message}
+                                {error && (
+                                    <div className="text-red-600 border border-red-400 rounded bg-red-100 px-4 py-3 mt-3">
+                                        {error.message}
+                                    </div>
+                                )}
+                            </div>
                         </div>
-                    )}
+                    </div>
                 </div>
-            </div>
+            </main>
         </div>
-    </div>
-</main>
 
     );
 };
