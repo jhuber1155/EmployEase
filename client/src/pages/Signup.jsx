c241a06561c2f9ae38238988f3f088daaa7f03a4
afong <adam.y.w.fong@gmail.com>
2023-11-21 12:06:35 -0800

    added back loading icon to signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -92,7 +92,7 @@ const Signup = () => {
                                             />
                                         </div>
                                         <button className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full" type="submit">
-                                            Submit
+                                            {loading ? loadingIcon : "Submit"}
                                         </button>
                                     </form>
                                 )}
